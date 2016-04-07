0ec871df8b16d280c97cabbe7c6233153af404d7
smithsagi <smitha.smile@gmail.com>
2016-04-07 12:57:17 -0500

    the beginning ...

---
+++ samples1/src/main/java/com/javasmith/samples1/pangrams/Solution.java
@@ -0,0 +1,27 @@
+package com.javasmith.samples1.pangrams;
+
+/**
+ * Created by Smitha Paramanandan on 4/7/2016.
+ * (Pangrams are sentences constructed by using every letter of the alphabet at least once.)
+ *
+ * Given a sentence s, tell Roy if it is a pangram or not.
+ * Input Format: Input consists of a string s.
+ * Constraints: Length of s can be at most 103103 (1≤|s|≤103)(1≤|s|≤103) and
+ * it may contain spaces, lower case and upper case letters. Lower-case and upper-case instances of a
+ * letter are considered the same.
+ * Output Format: Output a line containing pangram if s is a pangram, otherwise output not pangram.
+ * Sample Input:
+ * Input #1
+ * "We promptly judged antique ivory buckles for the next prize"
+ * Input #2
+ * "We promptly judged antique ivory buckles for the prize"
+ * Sample Output:
+ * Output #1
+ * pangram
+ * Output #2
+ * not pangram
+ * Explanation: In the first test case, the answer is pangram because the sentence contains all the letters
+ * of the English alphabet.
+ */
+public class Solution {
+}
